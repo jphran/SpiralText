10388985b3d30c61027b72dc634aef6b1f19d783
jphran <justinhfrancis@gmail.com>
2019-09-08 17:20:21 -0600

    Working, seperated main from harupdf

--- A2/HaruPDF.cpp
+++ A2/HaruPDF.cpp
@@ -29,23 +29,48 @@ HaruPDF::HaruPDF(int argc, char **argv)
 void HaruPDF::writeToPDF(double xPos, double yPos, double angleRad, char toWrite)
 {
   /* text along a circle */
-  char buf[2];
+  double angle2 = 180;
+  double rad1;
+  double rad2;
+  unsigned int i;
+  const char* sample = "sample text the quick brown fox jumped";
+
   HPDF_Page_BeginText (page);
   // Their example sets font twice. Probably some kind of mistake. Fix it or do what they do.
   font = HPDF_GetFont (pdf, "Courier-Bold", NULL);
   HPDF_Page_SetFontAndSize (page, font, 30);
+  // Place characters one at a time on the page.
+  for (i = 0; i < strlen (sample); i++) {
+      char buf[2];
+      float x;
+      float y;
+      // rad1 determines the angle of the letter on the page. rad2 is how far
+      // around the circle you are. Notice that they are perpendicular and
+      // thus not independent.
+      //
+      // Pay careful attention to what wants radians and what is degrees
+      // between haru and spiral and math functions.
+      rad1 = (angle2 - 90) / 180 * 3.141592;
+      rad2 = angle2 / 180 * 3.141592;
 
-  // This ugly function defines where any following text will be placed
-  // on the page. The cos/sin stuff is actually defining a 2D rotation
-  // matrix.
-  HPDF_Page_SetTextMatrix(page,
-                          cos(angleRad), sin(angleRad), -sin(angleRad), cos(angleRad),
-                          xPos, yPos);
+      // The position of the character depends on the center point
+      // plus the angle and the radius.
+      x = 210 + cos(rad2) * 150;
+      y = 300 + sin(rad2) * 150;
 
-  // C-style strings are null-terminated. The last character must a 0.
-  buf[0] = toWrite; // The character to display
-  buf[1] = 0;
-  HPDF_Page_ShowText (page, buf);
+      // This ugly function defines where any following text will be placed
+      // on the page. The cos/sin stuff is actually defining a 2D rotation
+      // matrix.
+      HPDF_Page_SetTextMatrix(page,
+                              cos(rad1), sin(rad1), -sin(rad1), cos(rad1),
+                              x, y);
+
+      // C-style strings are null-terminated. The last character must a 0.
+      buf[0] = sample[i]; // The character to display
+      buf[1] = 0;
+      HPDF_Page_ShowText (page, buf);
+      angle2 -= 10.0; // change the angle around the circle
+  }
 }
 
 void HaruPDF::savePDF(void)
--- A2/spiralPDF.cpp
+++ A2/spiralPDF.cpp
@@ -42,37 +42,7 @@ a close copy of pdfExample.cpp
 int main (int argc, char ** argv)
 {
     HaruPDF hp(argc, argv);
-
-//************spiral*************
-    unsigned int i;
-    const char* sample = "exmaple text";
-    double angle2 = 180;
-    double rad1;
-    double rad2;
-
-    // Place characters one at a time on the page.
-    for (i = 0; i < strlen (sample); i++) {
-        float x;
-        float y;
-        // rad1 determines the angle of the letter on the page. rad2 is how far
-        // around the circle you are. Notice that they are perpendicular and
-        // thus not independent.
-        //
-        // Pay careful attention to what wants radians and what is degrees
-        // between haru and spiral and math functions.
-        rad1 = (angle2 - 90) / 180 * 3.141592;
-        rad2 = angle2 / 180 * 3.141592;
-
-        // The position of the character depends on the center point
-        // plus the angle and the radius.
-        x = 210 + cos(rad2) * 150;
-        y = 300 + sin(rad2) * 150;
-        // This ugly function defines where any following text will be placed
-        // on the page. The cos/sin stuff is actually defining a 2D rotation
-        // matrix.
-        hp.writeToPDF(x,y,rad1,sample[i]);
-        angle2 -= 10;
-  }
+    hp.writeToPDF(1,1,1, 'a');
     hp.savePDF();
 
     return 0;
