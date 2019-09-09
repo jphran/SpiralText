6f069979481b8eb98e3d64442637c51e97cada29
jphran <justinhfrancis@gmail.com>
2019-09-08 18:25:24 -0600

    working on spiral class

--- A2/Spiral.cpp
+++ A2/Spiral.cpp
@@ -9,3 +9,36 @@ Modified:
 
 Description: A class for generating coordinates for a spiral
 */
+
+
+#include <cmath>
+#include "Spiral.h"
+
+Spiral::Spiral(double centerX, double centerY, double startRadius, double startAngDeg)
+ : centerX_(centerX), centerY_(centerY), startRadius_(startRadius), startAngDeg_(startAngDeg) {}
+
+/*
+  internally advances the spiral to its next position.
+  works for spirals that fit on one 8.5 x 11in page.
+*/
+void Spiral::operator++()
+{
+  //TODO:
+}
+
+double Spiral::getTextX()
+{
+  return textX_;
+}
+
+double Spiral::getTextY()
+{
+  return textY_;
+}
+
+
+//returns degrees
+double Spiral::getTextAngle()
+{
+  return textAngDeg_;
+}
--- A2/Spiral.h
+++ A2/Spiral.h
@@ -8,12 +8,12 @@ Modified:
 
 */
 
-#ifndef SPIRALTEXT_H
-#define SPIRALTEXT_H
+#ifndef SPIRAL_H
+#define SPIRAL_H
 
 #include <cmath>
 
-Class SpiralText
+class Spiral
 {
 private:
   double centerX_;
@@ -25,34 +25,11 @@ private:
   double textAngDeg_;
 
 public:
-  SpiralText(double centerX, double centerY, double startRadius, double startAngDeg
-  : centerX_(centerX), centerY_(centerY), startRadius_(startRadius), startAngDeg_(startAngDeg)){}
-
-  /*
-    internally advances the spiral to its next position.
-    works for spirals that fit on one 8.5 x 11in page.
-  */
-  void operator++()
-  {
-    //TODO:
-  }
-
-  double getTextX()
-  {
-    return textX_;
-  }
-
-  double getTextY()
-  {
-    return textY_;
-  }
-
-
-  //returns degrees
-  double getTextAngle()
-  {
-    return textAngDeg_;
-  }
+  Spiral(double centerX, double centerY, double startRadius, double startAngDeg);
+  void operator++();
+  double getTextX();
+  double getTextY();
+  double getTextAngle();
 
 };
 #endif
